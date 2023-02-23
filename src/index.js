5b8e0ac76bc5ef3d181bed7c76c31dacc47ae5f7
pvillanuevatti <peter.villanueva@t>
2023-02-23 10:31:36 +0800

    update route

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Exercise2 from "./pages/Exercise2";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as HashRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
 const root = ReactDOM.createRoot(document.querySelector("#fa-assessment-e1"));
 root.render(
-  <Router basename={process.env.PUBLIC_URL}>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path="/" element={<App></App>}></Route>
       <Route path="/exercise-2" element={<Exercise2></Exercise2>}></Route>
     </Routes>
-  </Router>
+  </HashRouter>
 );
